376e573800237fc5d032e3e33bcbeaefe83a9fac
ConstantJames <jamesconstantine13519@gmail.com>
2024-08-26 21:10:39 -0700

    Added Lesson B content

---
+++ JConstantine_Unit1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * 10);
+    }
+}
