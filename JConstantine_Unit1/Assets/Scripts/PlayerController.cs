a8a6d919b00a3d2171bb256e38aa31d42a88db88
ConstantJames <jamesconstantine13519@gmail.com>
2024-09-01 17:26:24 -0700

    Unit 1 Lesson D Commit

--- JConstantine_Unit1/Assets/Scripts/PlayerController.cs
+++ JConstantine_Unit1/Assets/Scripts/PlayerController.cs
@@ -5,17 +5,31 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 15.0f;
+    // Sets speed of vehicle
+    private float speed = 15.0f;
+    private float turnSpeed = 45.0f;
+
+    // Sets player inputs
+    private float horizontalInput;
+    private float verticalInput;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed );
+       // Makes player input work
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+       // Vehicle Forward/Backwards
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput );
+       // Vehicle Turn
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+
     }
 }
