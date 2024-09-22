4dbcbb03fcb25ebc48af7b0d7339b62113083902
ConstantJames <jamesconstantine13519@gmail.com>
2024-09-22 16:35:35 -0700

    Unit 2 Lesson C Commit

---
+++ JConstantine_Unit2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    public GameObject[] animalPrefabs;
+    private float xPosRange = 13;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            float randXPos = Random.Range(-xPosRange, xPosRange);
+            int animalPrefabIndex = Random.Range(0, animalPrefabs.Length);
+            Vector3 randPos = new Vector3(randXPos, 0, 21);
+            Instantiate(animalPrefabs[animalPrefabIndex], new Vector3( randXPos, 0, 21), animalPrefabs[animalPrefabIndex].transform.rotation);
+        }
+    }
+}
