63618ba13909596d7321fc1c1c911daa3cd83d6a
ConstantJames <jamesconstantine13519@gmail.com>
2024-11-03 17:53:13 -0800

    Unit 4 Lesson C

--- JConstantine_Unit4/Assets/Scripts/PlayerController.cs
+++ JConstantine_Unit4/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,10 @@ public class PlayerController : MonoBehaviour
     GameObject focalPoint;
     Renderer rendererPlayer;
     public float speed = 10.0f;
+    public float powerupSpeed = 10.0f;
+    public GameObject powerupInd;
+
+    public bool hasPowerup = false;
     
     // Start is called before the first frame update
     void Start()
@@ -32,5 +36,40 @@ public class PlayerController : MonoBehaviour
         {
             rendererPlayer.material.color = new Color(1.0f + forwardInput, 1.0f, 1.0f + forwardInput);
         }
+        powerupInd.transform.position = transform.position;
     }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Powerup"))
+            {
+               
+            hasPowerup = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdown());
+            powerupInd.SetActive(true);
+            }
+
+
+        }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (hasPowerup && collision.gameObject.CompareTag("Enemy")) 
+        {
+            Debug.Log("Player has collided with" + collision.gameObject + " with powerup set to: " + hasPowerup);
+            Rigidbody rbEnemy = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayDir = collision.gameObject.transform.position - transform.position;
+
+            rbEnemy.AddForce(awayDir * powerupSpeed, ForceMode.Impulse);
+
+        }
+    }
+
+    IEnumerator PowerupCountdown()
+    {
+        yield return new WaitForSeconds(8);
+        hasPowerup = false;
+        powerupInd.SetActive(false);
+    }
+
 }
